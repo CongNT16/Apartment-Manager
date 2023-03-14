0624cbc873d350f48659721d16203f3fd5961aba
Laputa <nguyencong160602@gmail.com>
2023-03-14 21:10:52 +0700

    Create component
    Create component

---
+++ ApaMan/web/assets/js/room-control.js
@@ -0,0 +1,74 @@
+const REGEX_NUMBER = '^[1-9][0-9]{0,6}$';
+const REGEX_NAME = '^[a-zA-Z0-9,.]+$';
+const REGEX_ROOM_NAME = '^[1-9][0-9]{2}[A-Z]{1}$';
+document.getElementById('delete-floor-button-block').style.display = 'none';
+
+function openConfirmDeleteFloor(floorId, floorName) {
+    document.getElementById('delete-floor-button-block').style.display = 'block';
+    document.getElementById('update-floor-button-block').style.display = 'none';
+    document.getElementById('floor-name-delete').innerText = floorName + ' ?';
+    document.querySelector('#delete-floor-button-block a').href = 'floor-management?submitType=Delete&floorId=' + floorId;
+}
+
+function closeConfirmDeleteFloor() {
+    document.getElementById('delete-floor-button-block').style.display = 'none';
+    document.getElementById('update-floor-button-block').style.display = 'block';
+    document.getElementById('floor-name-delete').innerText = '';
+    document.querySelector('#delete-floor-button-block a').href = '';
+}
+
+function checkFloorNameDuplicate() {
+    let floorNames = document.querySelectorAll('.floor-name');
+    let flagExist = false;
+    let flagEmpty = false;
+    let flagNegative = true;
+    for (let i = 0; i < floorNames.length - 1; ++i) {
+        if (floorNames[i].value.trim() === '') {
+            flagEmpty = true;
+            break;
+        } 
+        if (!floorNames[i].value.match(REGEX_NUMBER)){
+            flagNegative = false;
+            break;
+        }
+        for (let j = i + 1; j < floorNames.length; ++j) {
+            if (floorNames[i].value === floorNames[j].value) {
+                /*Tìm thấy 1 phần tử trùng là đủ và dừng vòng lặp*/
+                flagExist = true;
+                break;
+            }
+        }
+    }
+    if (flagEmpty) {
+        showToast('warning', 'APAMAN Notification', 'Floor\'s name Empty');
+    } else if (!flagNegative) {
+        showToast('warning', 'APAMAN Notification', 'Floor\'s name must be positive number');
+    } else if (flagExist) {
+        showToast('warning', 'APAMAN Notification', 'Floor\'s name must be unique');
+    } else {
+        document.getElementById('update-floor-form').submit();
+    }
+}
+
+function validateAddFloorName() {
+    const floorName = document.getElementById('add-floor-name').value;
+    if (floorName.trim() === '') {
+        showToast('warning', 'APAMAN Notification', 'Floor\'s name Empty');
+    } else if (!floorName.match(REGEX_NUMBER)) {
+        showToast('warning', 'APAMAN Notification', 'Floor\'s name must be positive number');
+    } else {
+        document.getElementById('add-floor-form').submit();
+    }
+}
+
+function validateAddRoomName() {
+    const roomName = document.getElementById('add-name-room').value;
+    if (roomName.trim() === '') {
+        showToast('warning', 'APAMAN Notification', 'Room\'s name Empty');
+    } else if (!roomName.match(REGEX_ROOM_NAME)) {
+        showToast('warning', 'APAMAN Notification', 'Room\'s name is malformed (101A)');
+    } else {
+        document.getElementById('add-room-form').submit();
+    }
+}
+
